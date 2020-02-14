20af2b1efb9ac43a11e72da02008b38e00de97c9
Thomas John Helin <thomashelin@yahoo.com>
2020-02-14 22:26:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.length == 1 
+  array.join
+elsif array.length == 2
+  array.join(" and ")
+elsif array.length > 2
+  array[-1].insert(0,"and ") #first we grab #array at the index of -1(which is the last item in the array), then we use the insert method to add "and " at the index of 0 for that final string.
+  array.join(", ")
+ end
 end
